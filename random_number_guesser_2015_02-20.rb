00e979f5ef95259de7b5a840348f2d845d6c2d00
Grace Yu <grace.yu4444@gmail.com>
2015-02-20 19:23:12 -0700

    adding methods

--- random_number_guesser_2015_02-20.rb
+++ random_number_guesser_2015_02-20.rb
@@ -1,18 +1,25 @@
 #!/usr/bin/env ruby
 
-number = rand(1..10) + 1
-
-print 'What is your guess? '
-guess = gets.chomp.to_i
-
-while guess != number
-  if guess > number
-    puts "Too high"
-  else guess < number
-    puts "Too low"
-  end
+def ask_for_guess
   print 'What is your guess? '
-  guess = gets.chomp.to_i
-  break if guess == number
+  @guess = gets.chomp.to_i
 end
-puts "You got it!"
+
+def analyze_guess(guess)
+  number = rand(1..10) + 1
+
+  ask_for_guess
+  while @guess != number
+    if @guess > number
+      puts "Too high"
+    else
+      @guess < number
+      puts "Too low"
+    end
+    ask_for_guess
+    break if @guess == number
+  end
+  puts "You got it!"
+end
+
+puts analyze_guess(@number)
